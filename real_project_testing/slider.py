cd2f62c4fff0c3ec639e509bab20b6f9122b07b4
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2025-03-11 14:49:32 +0600

    normal slider

--- real_project_testing/slider.py
+++ real_project_testing/slider.py
@@ -8,21 +8,21 @@ def browser():
         context = browser.new_context()
         yield context
         browser.close()
-# def test_slider(browser):
-#     page = browser.new_page()
-#     page.close()
-#     page.goto("https://demoqa.com/slider")
-#     page.wait_for_load_state("domcontentloaded")
-#     slider = page.locator("input.range-slider")
-#     slider.wait_for(state="visible")
-#     slider_x = slider.bounding_box()["x"]
-#     slider_y = slider.bounding_box()["y"]
-#     page.mouse.move(slider_x, slider_y)
-#     page.mouse.down()
-#     page.mouse.move(slider_x + 150, slider_y)
-#     page.mouse.up()
-#     page.wait_for_timeout(4000)
-#     page.close()
+def test_slider(browser):
+    page = browser.new_page()
+    page.close()
+    page.goto("https://demoqa.com/slider")
+    page.wait_for_load_state("domcontentloaded")
+    slider = page.locator("input.range-slider")
+    slider.wait_for(state="visible")
+    slider_x = slider.bounding_box()["x"]
+    slider_y = slider.bounding_box()["y"]
+    page.mouse.move(slider_x, slider_y)
+    page.mouse.down()
+    page.mouse.move(slider_x + 150, slider_y)
+    page.mouse.up()
+    page.wait_for_timeout(4000)
+    page.close()
 
 def test_range_slider(browser):
     page = browser.new_page()
